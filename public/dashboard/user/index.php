baaabf9e4aa78e42c265b4e56e025deb9a7a55eb
eldiKryeziu <eldikryeziu01@gmail.com>
2026-04-27 22:06:06 +0200

    .

--- public/dashboard/user/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../authentication.php';
-kontrolloQasjen('user'); // Lejon vetëm institucionet
-?>
-<h1>Mirësevini Institucion: <?php echo $_SESSION['emri']; ?></h1>
-<p>Këtu mund të ngarkoni dokumentet e vetëvlerësimit.</p>
-<a href="../../logout.php">Dilni (Logout)</a>
